f3524dcb7bad79b3cb224f0f9ce063dd923c8928
Michael Bender <michaelcb100@proton.me>
2026-06-02 21:41:36 -0400

    various changes made but never pushed

--- Shinobot.py
+++ Shinobot.py
@@ -1,4 +1,5 @@
-#pipreqs --encoding=utf8 .\Shinobot\ --force
+# pipreqs --encoding=utf8 .\Shinobot\ --force
+# pip install -r requirements.txt
 
 import discord
 import os
@@ -6,37 +7,47 @@ import random
 import socket
 import pickle
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext, utils
+# from discord_slash import SlashCommand, SlashContext, utils
 
 from complexCommands import init
+from complexCommands import ripCommands
+# from complexCommands import anime
 init.init()
-from complexCommands import rip
-#from complexCommands import anime
 
 path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(path)
 
-#client  = discord.Client()
-client 	= commands.Bot(command_prefix="!", intents = discord.Intents.all())
-slash 	= SlashCommand(client, sync_commands=True)
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+for x in intents:
+	if(not x[1]): print(x)
+print(intents.value)
+# client = discord.Bot()
+client = commands.Bot(command_prefix="!", intents = intents)
+# slash = SlashCommand(client, sync_commands=True)
+
+# Time after which to delete a message (if it is intended to be temporary)
 cooldownTime = 15
 
-TestingGuildList = None 	#For Testing purposes, make into an array with single test guild id
-TestingGuild = None 		#For Testing purposes, set to test guild id
+# For Testing purposes
+# Set as test guild id and list with single test guild id
+TestingGuild = 357309832484880394
+TestingGuildList = [357309832484880394]
 
-#                 WizardRason#6819
-owner_user  = init.owner_user 	#settings["owner_user"]
-token       = init.token 		#settings["token"]
+# Bot owner ID and Bot token
+owner_user = init.owner_user
+token      = init.token
 
 admin_server = init.admin_server	#settings["admin_server"]
 
-#with open('jsonFiles/channels.json') as f:
+# with open('jsonFiles/channels.json') as f:
 try: channels = pickle.load( open( "jsonFiles/channels.p", "rb" ) )
 except EOFError: channels = {}
 
 @client.event
 async def on_ready():
-	print('Logged in as: '+ client.user.name + " (" + str(client.user.id) + ")")
+	print('Logged in as: ' + client.user.name + " (" + str(client.user.id) + ")")
 	print("Owner User ID: " + str(owner_user))
 	for server in client.guilds:
 		print ("Connected to server: {}".format(server))
@@ -59,54 +70,45 @@ async def on_message(message):
 
 	if (client.user.id != message.author.id and any(x in message.content.lower() for x in ["doughnut", "donut"])):
 		doughLinks = [
-		"https://thumbs.gfycat.com/MiserlyNippyCockroach-size_restricted.gif",
-		"https://i.imgur.com/16GrVjp.gif",
-		"https://i.imgur.com/1kzxKmJ.gif",
-		"http://i0.kym-cdn.com/photos/images/original/000/758/438/be5.gif",
-		"https://thumbs.gfycat.com/ImaginativeRadiantAldabratortoise-size_restricted.gif",
-		"http://i0.kym-cdn.com/photos/images/original/001/089/184/f86.gif",
-		"https://78.media.tumblr.com/9642f23f4b95da57a9ea4c835e395e7a/tumblr_oompyrlBxh1r922azo1_540.gif"
-		]
+			"https://thumbs.gfycat.com/MiserlyNippyCockroach-size_restricted.gif",
+			"https://i.imgur.com/16GrVjp.gif",
+			"https://i.imgur.com/1kzxKmJ.gif",
+			"http://i0.kym-cdn.com/photos/images/original/000/758/438/be5.gif",
+			"https://thumbs.gfycat.com/ImaginativeRadiantAldabratortoise-size_restricted.gif",
+			"http://i0.kym-cdn.com/photos/images/original/001/089/184/f86.gif",
+			"https://78.media.tumblr.com/9642f23f4b95da57a9ea4c835e395e7a/tumblr_oompyrlBxh1r922azo1_540.gif"
+			]
 		doughPhrases = [
-		"Did someone say DONUTS!?!?!?!",
-		"Just looking at it reveals how delicious it must be.\nI can tell! I don't even have to eat it.\nI will, though!",
-		"Please present those donuts to me immediately!",
-		"I haven't tasted it yet, but just imagining the flavor, I can almost feel it expanding in my mouth, and I'm certain that I'll shout, without being afraid of what others might think...\nJAPANAINO!",
-		"Flocky chou?",
-		"Panaino!",
-		"Are they donuts? They're donuts, right? They have to be donuts!",
-		"So they are donuts! That's magnificent!"
-		]
-		#msg_donut = 
+			"Did someone say DONUTS!?!?!?!",
+			"Just looking at it reveals how delicious it must be.\nI can tell! I don't even have to eat it.\nI will, though!",
+			"Please present those donuts to me immediately!",
+			"I haven't tasted it yet, but just imagining the flavor, I can almost feel it expanding in my mouth, and I'm certain that I'll shout, without being afraid of what others might think...\nJAPANAINO!",
+			"Flocky chou?",
+			"Panaino!",
+			"Are they donuts? They're donuts, right? They have to be donuts!",
+			"So they are donuts! That's magnificent!"
+			]
+
 		await message.channel.send(random.choice(doughPhrases) + "\n" + random.choice(doughLinks), delete_after = cooldownTime)
-		#asyncio.ensure_future(deleteAfterTime(msg_donut, cooldownTime))
 
-	#posts a meme based on the given search term, or posts a default meme (meme.jpg)
-	elif (message.content.startswith('!meme') and False): #not done yet
-		if len(message.content.strip()) > 5:
-			meme = message.content[5:]
-			await message.channel.send('This is a meme. Searching the web for:' + meme)
-		else:
-			await message.channel.send('meme.jpg')
-
-	elif (message.content.startswith('!motto')): 
+	#SwS motto
+	if (message.content.startswith('!motto')): 
 		mottos = ["Retarded, not gay!", "Retarded, not Noobscout!"]
 		await message.channel.send(random.choices(mottos,weights=(19,1))[0])
 
 	#posts a gif of shinobu being carried
-	elif (message.content.startswith('!adult')):
+	if (message.content.startswith('!adult')):
 		await message.channel.send('I need an adult\n' + "https://thumbs.gfycat.com/BigheartedSplendidFlyingfish-size_restricted.gif")
 
 	#!rip commands
-	elif (message.content.startswith('!rip')):
-		await rip.commandRip(message, client, owner_user)
+	if (message.content.startswith('!rip')):
+		await ripCommands.commandRip(message, client, owner_user)
 		
 	#gives the link to the mal page of the given anime
-	elif (message.content.startswith('!anime') or message.content.startswith('!manga')):
+	#elif (message.content.startswith('!anime') or message.content.startswith('!manga')):
 		#await (anime.commandAnime(message, client) if message.content[1] == 'a' else anime.commandManga(message, client))
-		pass
 
-	elif (message.content.startswith('!command')):
+	if (message.content.startswith('!command')):
 		cmd = message.content[8:].strip() if len(message.content.strip()) > 7 else None
 		if cmd:
 			scriptdir = path+"/scripts/"
@@ -114,13 +116,13 @@ async def on_message(message):
 				os.system(scriptdir + cmd)
 			else: await message.channel.send("Bad Path")
 
-	elif (message.content.startswith('!cmdlist')):
+	if (message.content.startswith('!cmdlist')):
 		if os.path.exists('scripts/'):
 			l = os.listdir(path + '/scripts/')
 			await message.channel.send([x for x in l if not '@' in x])
 			if message.author.id == owner_user: await message.channel.send(l)
 		else: await message.channel.send("None Saved")
-
+'''
 	#posts everything said in public_channel into secret_channel, including who said it
 	if (message.channel.type == discord.ChannelType.text and message.guild != client.get_guild(admin_server) or (client.user.id != message.author.id and message.channel.id in list(channels.keys()))):
 		if message.channel.id not in list(channels.keys()) and message.guild != client.get_guild(admin_server):
@@ -141,6 +143,7 @@ async def on_message(message):
 				try: await client.get_channel(channels[message.channel.id]).send(content = content)
 				except AttributeError: pass
 '''
+
 options = [
 	{
 		'name' 			: "optone",
@@ -159,24 +162,29 @@ options = [
 		]
 	}
 ]
+testlist = [discord.OptionChoice("ChoiceOne","DOGE!"),discord.OptionChoice("ChoiceTwo","NO DOGE")]
 
-@slash.slash(name="test", description = "This is just a test command, nothing more.", guild_ids = TestingGuildList, options = options)
-async def test(ctx: SlashContext, optone: str):
+@client.slash_command(name="test", description = "This is just a test command, nothing more.", guild_ids = TestingGuildList)#, options = options)
+async def test(ctx, optone: discord.Option(str,description = "This is the first option we have.",choices=testlist)):
 	global options
 	embed = discord.Embed(title="embed test")
+	print(testlist)
 	await ctx.send(content=optone, embeds=[embed])
-	await ctx.send(content="Here is a hidden message, just for you",hidden=True)
+	await ctx.respond(content="Here is a hidden message, just for you",ephemeral=True)
 	#makes changes to the options variable(adding more choices) and updates the command
-	options[0]['choices'].append({'name' : optone,'value' : "NO DOGE"})
-	await utils.manage_commands.add_slash_command(bot_id=client.user.id,bot_token=token,guild_id=TestingGuild,cmd_name="test", description = "This is just a test command, nothing more.",options=options)
-#'''
+	testlist.append(discord.OptionChoice("ChoiceThree","THE SECRET DOGE HIDING UNDER YOUR BED!"))
+	print(testlist)
+	ctx.command.update()
+	#await client.sync_commands(force=True)
+	#await utils.manage_commands.add_slash_command(bot_id=client.user.id,bot_token=token,guild_id=TestingGuild,cmd_name="test", description = "This is just a test command, nothing more.",options=options)
+'''
 ripOptions = [
 	{
 		"name"			: "name",
 		"description"	: "The 'rip' that you want",
 		"required"		: False,
 		"type"			: 3,
-		'choices' 		: rip.ripChoicesList()
+		'choices' 		: ripCommands.ripChoicesList()
 	}
 ]
 
@@ -207,13 +215,17 @@ ripRemoveOptions = [
 		"description"	: "The 'rip' that you want to remove",
 		"required"		: True,
 		"type"			: 3,
-		'choices' 		: rip.ripChoicesList()
+		'choices' 		: ripCommands.ripChoicesList()
 	}
 ]
 
-@slash.slash(name="rip", description = "A random rip phrase.", guild_ids = TestingGuildList, options = ripOptions)
-async def Randomrip(ctx: SlashContext, name = None):
-	await rip.commandRandomRip(name, ctx, owner_user)
+@client.slash_command(description = "A random rip phrase.", guild_ids = TestingGuildList, options = ripOptions)
+async def rip(
+	ctx: discord.ApplicationContext, 
+	name: discord.Option(str,"The descriptive name of the new 'rip'",default=None,choices=ripCommands.ripChoicesList())
+):
+	await ripCommands.commandRandomRip(name, ctx, owner_user)
+	await client.sync_commands()
 	ripOptions[0]['choices'] = rip.ripChoicesList()
 	await utils.manage_commands.add_slash_command(bot_id=client.user.id,bot_token=token,guild_id=TestingGuild,cmd_name="rip", description = "A random rip phrase.",options=ripOptions)
 
@@ -239,7 +251,7 @@ commandOptions = [
 	}
 ]
 
-@slash.slash(name="command", description = "Executes the given command, (ownder commands start with @).", guild_ids = TestingGuildList, options = commandOptions)
+@slash.slash(name="command", description = "Executes the given command, (owner commands start with @).", guild_ids = TestingGuildList, options = commandOptions)
 async def command(ctx: SlashContext, command : str):
 
 	scriptdir = path+"/scripts/"
@@ -250,6 +262,19 @@ async def command(ctx: SlashContext, command : str):
 	commandOptions[0]['choices'] = [{'name': k.split('.')[0], 'value' : k} for k in os.listdir(path + '/scripts/')]
 	await utils.manage_commands.add_slash_command(bot_id=client.user.id,bot_token=token,guild_id=TestingGuild,cmd_name="command", description = "Executes the given command, (ownder commands start with @).",options=commandOptions)
 
+sayOptions = [
+	{
+		"name"			: "say",
+		"description"	: "The command to perform",
+		"required"		: True,
+		"type"			: 3,
+		'choices' 		: [{'name': k.split('.')[0], 'value' : k} for k in os.listdir(path + '/scripts/')]
+	}
+]
+
+@slash.slash(name="say", description = "Bot says the thing.", guild_ids = TestingGuildList, options = sayOptions)
+async def command(ctx: SlashContext, say : str):
+	await ctx.send("Sorry, you don't have permission for that. Have some sparkles instead (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧")
 
 @slash.slash(name="adult", description = "I need an adult!", guild_ids = TestingGuildList)
 async def Adult(ctx: SlashContext):
@@ -260,6 +285,6 @@ async def Motto(ctx: SlashContext):
 	mottos = ["Retarded, not gay!", "Retarded, not Noobscout!"]
 	await ctx.send(random.choices(mottos,weights=(19,1))[0])
 
-
+#'''
 if __name__ == '__main__':
 	client.run(token)
